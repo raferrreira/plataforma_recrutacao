cc4d72cf2aee7aa3e304d0b6fd2a5f0cb3c920d1
raferrreira <raferrreira@outlook.com>
2019-06-11 13:59:54 -0300

    1st commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// toggle dropdown profile
+
+$(".profile-tab a").click(function(){
+	$(".profile-tab ul").toggle();
+})
+ 
+
+// modal abrir e fechar
+
+$(".open-modal").click(function(){
+	$("body").addClass("modal-open");
+	$(".modal-avaliacao-bg").addClass("active");
+})
+
+$(".close-btn").click(function(){
+	$("body").removeClass("modal-open");
+	$(".modal-avaliacao-bg").removeClass("active");
+})
+
+// smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// JavaScript inicial para desativar envios de formulário, se houver campos inválidos.
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+    // Pega todos os formulários que nós queremos aplicar estilos de validação Bootstrap personalizados.
+    var forms = document.getElementsByClassName('needs-validation');
+    // Faz um loop neles e evita o envio
+    var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+        }, false);
+    });
+    }, false);
+})();
